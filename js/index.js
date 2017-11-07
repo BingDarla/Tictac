04dc8ebf00b3822668bcebce5a3ea2b656f12455
IceMac <hbshuang@gmail.com>
2017-11-07 16:22:59 +1100

    game table version full

---
+++ js/index.js
@@ -0,0 +1,99 @@
+
+
+$(document).ready(function(){
+
+  ticTac.gameTable();
+
+  let currentPlayer = 1;
+  //function
+  console.log("The document is ready");
+
+  const gameReset = function(){
+    ticTac.reset();
+    $(".squres").removeClass("player1");
+    $(".squres").removeClass("player2");
+  }
+
+
+
+   $(".squres").click(function(){
+     if (ticTac.checkClick($(this).attr('id'))){
+       console.log('You can not cliked there');
+       return;
+     }
+
+     if(currentPlayer === 1 ){
+      // condition:check that grid is not clicked
+
+      $(this).addClass("player1");
+      console.log("player1 is called");
+      console.log($(this).attr('id'));
+      ticTac.player1Array.push($(this).attr('id'));
+      console.log("player1Array: "+ticTac.player1Array);
+
+      ticTac.totalArray.push($(this).attr('id'));
+
+      if (ticTac.player1Array.length >= 3){
+        ticTac.checkWinner(ticTac.player1Array);
+      console.log('player1Array was checked');
+        console.log("after check"+ticTac.winnerFound);
+        if (ticTac.winnerFound){
+          console.log(`YOU ARE THE WINNER1:${ticTac.player1Array}`);
+          ticTac.player1Score++;
+          $('#player1').text(ticTac.player1Score);
+          gameReset();
+          return;
+
+        }
+      }
+      ticTac.counter ++;
+      console.log(`counter: ${ticTac.counter}`);
+      if (ticTac.counter >=9){
+        console.log(`player1 reset: ${ticTac.counter}`)
+        gameReset();
+        return;
+      }
+      currentPlayer = 0;
+    }  else {
+      //condition: check the grid is not clicked
+
+      $(this).addClass("player2");
+      console.log("player2 is called");
+      ticTac.player2Array.push($(this).attr('id'));
+      console.log("player2Array:  "+ticTac.player2Array);
+
+      ticTac.totalArray.push($(this).attr('id'));
+
+      if (ticTac.player2Array.length >=3){
+        ticTac.checkWinner(ticTac.player2Array);
+        console.log('player2Array was checked');
+        console.log("after check"+ticTac.winnerFound);
+        if (ticTac.winnerFound){
+          console.log("YOU ARE THE WINNER2 "+ticTac.player2Array);
+          ticTac.player2Score++;
+          $('#player2').text(ticTac.player2Score);
+          gameReset();
+          return;
+
+        }
+      }
+      ticTac.counter ++;
+      console.log(`counter: ${ticTac.counter}`);
+      if (ticTac.counter >=9){
+        console.log(`player2 reset ${ticTac.counter}`)
+        gameReset();
+        return;
+      }
+      // ticTac.counter ++;
+      currentPlayer = 1;
+    }
+
+
+  })
+
+  //checking the playersWay
+
+  //ticTac.checkWinner([1,5,6,7]);
+  // ticTac.checkWinner(['1','5','9']);
+
+})
---
+++ js/tictac.js
@@ -0,0 +1,100 @@
+
+const size = 3;
+    // use for index of table
+
+
+  const ticTac  = {
+    winner:{
+      1:['1','2','3'],
+      2:['4','5','6'],
+      3:['7','8','9'],
+      4:['1','4','7'],
+      5:['2','5','8'],
+      6:['3','6','9'],
+      7:['1','5','9'],
+      8:['3','5','7']
+    },
+     player1Score:0,
+     player2Score:0,
+
+     totalArray:[],    // hold all the id players clicked.
+     winFlag:false,
+
+     winnerFound:false,
+           // true for win
+
+     counter:0,       //if all the grid are clicked
+
+     player1Array:[],  //hold all the click play1 did
+     player2Array:[],  //hold all the click play2 did
+     
+    //generate a talbe for Tic Tac Toe game based on size;
+    gameTable: function(){
+         let account = 1;
+      const parent = $('#game');
+      for (let i=0;i<size;i++){
+            let row = $('<tr>');
+            $(parent).append(row);
+            // console.log(row);
+            for (let j=0;j<size;j++){
+              col = $('<td>')
+              $(row).append(col);
+              $(col).attr("id",account).attr("class","squres");
+              account++;
+            }
+
+      }
+        // console.log("gameTable is running");
+    },
+    //checking function if playersArray contain Winner array
+    checkWinner:function( playersArray ){
+      console.log("checking function was called");
+      for (key in this.winner) {
+        let winArray = this.winner[key];
+        let found = true;
+
+        for (let i=0;i<winArray.length;i++){
+          this.winFlag = false;
+          for (let j=0; j<playersArray.length;j++){
+            if (winArray[i] === playersArray[j]){
+              this.winFlag = true;
+              break;
+            }
+          }
+          if (this.winFlag === false){
+            found = false;
+            break;
+        }
+
+        }
+        if (found === true){
+          console.log("You win!!");
+          console.log(this.winFlag);
+          this.winnerFound = true;
+          return;
+          //alert("You win");
+        }
+      }
+  },
+      //checking the grid was clicked or not
+      checkClick:function(id){
+        console.log('checkClick function is called');
+        console.log("Total "+this.totalArray);
+        for (let i = 0; i<this.totalArray.length; i++){
+          if (id ===this.totalArray[i])
+          return true;
+        }
+      },
+
+    //reset function
+     reset:function(){
+      this.counter = 0;
+      this.player1Array=[];
+      this.player2Array=[];
+      this.totalArray = [];
+      ticTac.winnerFound = false;
+      console.log("reset was called!!!!!");
+      console.log(this.player1Array+"   "+this.player2Array);
+     }
+
+  }
