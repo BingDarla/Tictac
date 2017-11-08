1d2e4132dc91864fbd68b29f89ec33c0c11f34ab
IceMac <hbshuang@gmail.com>
2017-11-08 16:15:18 +1100

    best version GUI need to do

--- js/index.js
+++ js/index.js
@@ -1,85 +1,149 @@
-
-
-$(document).ready(function(){
+$(document).ready(function() {
 
   ticTac.gameTable();
 
-  let currentPlayer = 1;
-  
+  let currentPlayer1 = 0;
+  let currentPlayer2 = 0;
+  let playFlag1 = 0;
+  let playFlag2 = 0;
+
   //function
   console.log("The document is ready");
 
-  let flag = false;  //  game over turn true;
-  const gameReset = function(){
+  let flag = false; //  game over turn true;
+  //gamereset function
+  const gameReset = function() {
     ticTac.reset();
     $(".squres").removeClass("player1");
     $(".squres").removeClass("player2");
+    playFlag1 = currentPlayer1;
+    playFlag2 = 0;
   }
-     $('#p4').click(function(){
-       gameReset();
-       flag = false;
-     })
+  //reset button
+  $('#p4').click(function() {
+    gameReset();
+    flag = false;
+  })
+
+  $('#p1').click(function() {
+    currentPlayer1 = 1;
+    playFlag1 = 1;
+    console.log('player1 is choosen');
+  })
+  $('#p2').click(function() {
+    currentPlayer2 = 1;
+    console.log('player2 is choosen')
+  })
 
 
-   $(".squres").click(function(){
-     if (ticTac.checkClick($(this).attr('id'))){
-       console.log('You can not cliked there');
-       return;
-     }
-     if (flag){
-       return;
-     }
 
-     if(currentPlayer === 1 ){
+
+
+
+  $(".squres").click(function() {
+    if (ticTac.checkClick($(this).attr('id'))) {
+      console.log('You can not click there');
+      return;
+    }
+    if (flag) {
+      return;
+    }
+
+    if (playFlag1 === 1 && !playFlag2) {
       // condition:check that grid is not clicked
 
       $(this).addClass("player1");
       console.log("player1 is called");
-      console.log($(this).attr('id'));
-      ticTac.player1Array.push($(this).attr('id'));
-      console.log("player1Array: "+ticTac.player1Array);
+      // console.log($(this).attr('id'));
+      ticTac.player1Array.push(+$(this).attr('id'));
+      // console.log("player1Array: " + ticTac.player1Array);
 
-      ticTac.totalArray.push($(this).attr('id'));
+      ticTac.totalArray.push(+$(this).attr('id'));
+      console.log('After player1 totalArray is :'+ticTac.totalArray);
 
-      if (ticTac.player1Array.length >= 3){
+      if (ticTac.player1Array.length >= 3) {
         ticTac.checkWinner(ticTac.player1Array);
-      console.log('player1Array was checked');
-        console.log("after check"+ticTac.winnerFound);
-        if (ticTac.winnerFound){
+        console.log('player1Array was checked');
+        console.log("after check" + ticTac.winnerFound);
+        if (ticTac.winnerFound) {
           console.log(`YOU ARE THE WINNER1:${ticTac.player1Array}`);
           ticTac.player1Score++;
           $('#player1').text(ticTac.player1Score);
-          flag = true;  //game over
+          flag = true; //game over
           // gameReset();
           return;
 
         }
       }
-      ticTac.counter ++;
+      ticTac.counter++;
       console.log(`counter: ${ticTac.counter}`);
-      if (ticTac.counter >=9){
+      if (ticTac.counter >= 9) {
         console.log(`player1 reset: ${ticTac.counter}`)
-        flag =true;
+        flag = true;
         // gameReset();
         return;
       }
-      currentPlayer = 0;
-    }  else {
+      if (currentPlayer2 === 1) {
+        playFlag1 = 0;
+        playFlag2 = 1;
+        console.log(`playFlag2 and playFlag1:   ${playFlag2}, ${playFlag1}`);
+      } else if (!currentPlayer2) {
+        //**********************AI AI AI**************
+        //***********************AI**get in********* count can reach 10
+        // console.log("AI function is called.");
+        let idAi = ticTac.AIplay();
+        $('#' + idAi).addClass("player2");
+        ticTac.player2Array.push(+idAi);
+        console.log("player2Array:  " + ticTac.player2Array);
+        ticTac.totalArray.push(+idAi);
+        console.log("After player 2, totalArray is "+ticTac.totalArray);
+        // checking AI whether win or not.
+        if (ticTac.player2Array.length >= 3) {
+          if (ticTac.checkWinner(ticTac.player2Array)){
+
+
+              console.log("YOU ARE THE WINNER2 " + ticTac.player2Array);
+              ticTac.AIscore++;
+              $('#player3').text(ticTac.AIscore);
+              flag = true;
+              // gameReset();
+              return;
+
+            }
+          }
+        }
+        ticTac.counter++;
+        console.log(`counter: ${ticTac.counter}`);
+        if (ticTac.counter >= 9) {
+          console.log(`player2 reset ${ticTac.counter}`)
+          flag = true;
+          // gameReset();
+          return;
+        }
+
+        playFlag1 = 1;
+
+
+
+      }
+
+//*******************Second player*******************
+     else if (playFlag2 ) {
       //condition: check the grid is not clicked
 
       $(this).addClass("player2");
       console.log("player2 is called");
-      ticTac.player2Array.push($(this).attr('id'));
-      console.log("player2Array:  "+ticTac.player2Array);
+      ticTac.player2Array.push(+$(this).attr('id'));
+      console.log("player2Array:  " + ticTac.player2Array);
 
-      ticTac.totalArray.push($(this).attr('id'));
+      ticTac.totalArray.push(+$(this).attr('id'));
 
-      if (ticTac.player2Array.length >=3){
+      if (ticTac.player2Array.length >= 3) {
         ticTac.checkWinner(ticTac.player2Array);
         console.log('player2Array was checked');
-        console.log("after check"+ticTac.winnerFound);
-        if (ticTac.winnerFound){
-          console.log("YOU ARE THE WINNER2 "+ticTac.player2Array);
+        console.log("after check" + ticTac.winnerFound);
+        if (ticTac.winnerFound) {
+          console.log("YOU ARE THE WINNER2 " + ticTac.player2Array);
           ticTac.player2Score++;
           $('#player2').text(ticTac.player2Score);
           flag = true;
@@ -88,24 +152,27 @@ $(document).ready(function(){
 
         }
       }
-      ticTac.counter ++;
+      ticTac.counter++;
       console.log(`counter: ${ticTac.counter}`);
-      if (ticTac.counter >=9){
+      if (ticTac.counter >= 9) {
         console.log(`player2 reset ${ticTac.counter}`)
         flag = true;
         // gameReset();
         return;
       }
-      // ticTac.counter ++;
-      currentPlayer = 1;
+
+      playFlag2 = 0;
+      playFlag1 = 1;
     }
 
 
   })
 
-  //checking the playersWay
-
-  //ticTac.checkWinner([1,5,6,7]);
-  // ticTac.checkWinner(['1','5','9']);
-
 })
+
+
+
+//checking the playersWay
+
+//ticTac.checkWinner([1,5,6,7]);
+// ticTac.checkWinner(['1','5','9']);
--- js/tictac.js
+++ js/tictac.js
@@ -1,105 +1,201 @@
-
 const size = 3;
-    // use for index of table
+// use for index of table
 
 
-  const ticTac  = {
-    winner:{
-      1:['1','2','3'],
-      2:['4','5','6'],
-      3:['7','8','9'],
-      4:['1','4','7'],
-      5:['2','5','8'],
-      6:['3','6','9'],
-      7:['1','5','9'],
-      8:['3','5','7']
-    },
-     player1Score:0,
-     player2Score:0,
+const ticTac = {
+  winner: {
+    1: ['1', '2', '3'],
+    2: ['4', '5', '6'],
+    3: ['7', '8', '9'],
+    4: ['1', '4', '7'],
+    5: ['2', '5', '8'],
+    6: ['3', '6', '9'],
+    7: ['1', '5', '9'],
+    8: ['3', '5', '7']
+  },
+  player1Score: 0,
+  player2Score: 0,
+  AIscore: 0,
 
-     totalArray:[],    // hold all the id players clicked.
-     winFlag:false,
+  totalArray: [], // hold all the id players clicked.
+  winFlag: false,
 
-     winnerFound:false,
-           // true for win
+  winnerFound: false,
+  // true for win
 
-     counter:0,       //if all the grid are clicked
+  counter: 0, //if all the grid are clicked
 
-     player1Array:[],  //hold all the click play1 did
-     player2Array:[],  //hold all the click play2 did
+  player1Array: [], //hold all the click play1 did
+  player2Array: [], //hold all the click play2 did
 
-    //generate a talbe for Tic Tac Toe game based on size;
-    gameTable: function(){
-         let account = 1;
-      const parent = $('#game');
-      for (let i=0;i<size;i++){
-            let row = $('<tr>');
-            $(parent).append(row);
-            // console.log(row);
-            for (let j=0;j<size;j++){
-              col = $('<td>')
-              $(row).append(col);
-              $(col).attr("id",account).attr("class","squres");
-              account++;
-            }
+  //generate a talbe for Tic Tac Toe game based on size;
+  gameTable: function() {
+    let account = 1;
+    const parent = $('#game');
+    for (let i = 0; i < size; i++) {
+      let row = $('<tr>');
+      $(parent).append(row);
+      // console.log(row);
+      for (let j = 0; j < size; j++) {
+        col = $('<td>')
+        $(row).append(col);
+        $(col).attr("id", account).attr("class", "squres");
+        account++;
+      }
+
+    }
+    // console.log("gameTable is running");
+  },
+  //checking function if playersArray contain Winner array
+  checkWinner: function(playersArray) {
+    console.log("checkingwinner function was called");
+    for (key in this.winner) {
+      let winArray = this.winner[key];
+      let found = true;
+
+      for (let i = 0; i < winArray.length; i++) {
+        this.winFlag = false;
+        for (let j = 0; j < playersArray.length; j++) {
+          if (+winArray[i] === +playersArray[j]) {
+            this.winFlag = true;
+            break;
+          }
+        }
+        if (this.winFlag === false) {
+          found = false;
+          break;
+        }
 
       }
-        // console.log("gameTable is running");
-    },
-    //checking function if playersArray contain Winner array
-    checkWinner:function( playersArray ){
-      console.log("checking function was called");
-      for (key in this.winner) {
-        let winArray = this.winner[key];
-        let found = true;
+      if (found === true) {
+        console.log("You win!!");
+        console.log(this.winFlag);
+        this.winnerFound = true;
+        return true;
+        //alert("You win");
+      }
+    }
+  },
+  //checking the grid was clicked or not
+  checkClick: function(id) {
+    console.log('checkClick function is called');
+    console.log("Total in checkClick:" + this.totalArray);
+    // debugger;
+    for (let i = 0; i < this.totalArray.length; i++) {
+      if (+id === +this.totalArray[i]){
 
-        for (let i=0;i<winArray.length;i++){
-          this.winFlag = false;
-          for (let j=0; j<playersArray.length;j++){
-            if (winArray[i] === playersArray[j]){
-              this.winFlag = true;
+          console.log('checkclick true'+id);
+          return true;
+      }
+
+    }
+       console.log('checkclick false'+id);
+       return false;
+  },
+
+  //reset function
+  reset: function() {
+    this.counter = 0;
+    this.player1Array = [];
+    this.player2Array = [];
+    this.totalArray = [];
+    ticTac.winnerFound = false;
+    console.log("reset was called!!!!!");
+    console.log(this.player1Array + "   " + this.player2Array);
+  },
+
+  //computer generate click id
+  AIplay: function() {
+    let remain = [];
+
+
+    if (!this.checkClick(5) ){
+      return 5;
+    }
+    //checking AI's going to win******************
+    if (this.player2Array.length >= 2) {
+      console.log('checking AI  is going to win')
+      for (key in this.winner) {
+
+        let counter1 = 0 // to record how close to win. 2 ==1 left to win
+        let winArray = this.winner[key];
+        for (let i = 0; i < winArray.length; i++) {
+          for (let j = 0; j < this.player2Array.length; j++) {
+            if (+winArray[i] === +this.player2Array[j] ){
+              counter1++;
               break;
             }
           }
-          if (this.winFlag === false){
-            found = false;
-            break;
-        }
 
         }
-        if (found === true){
-          console.log("You win!!");
-          console.log(this.winFlag);
-          this.winnerFound = true;
-          return;
-          //alert("You win");
+        if (counter1 === 2) {
+          console.log("find AI winner pattern:" +winArray);
+          for (let i = 0; i < winArray.length; i++) {
+            for (let j = 0; j < this.player2Array.length; j++) {
+              if (+winArray[i] === +this.player2Array[j])
+                break;
+            }
+
+            if ( !(this.checkClick( winArray[i]) )  )
+            return winArray[i];
+          }
         }
       }
-  },
-      //checking the grid was clicked or not
-      checkClick:function(id){
-        console.log('checkClick function is called');
-        console.log("Total "+this.totalArray);
-        for (let i = 0; i<this.totalArray.length; i++){
-          if (id ===this.totalArray[i])
-          return true;
-        }
-      },
-
-    //reset function
-     reset:function(){
-      this.counter = 0;
-      this.player1Array=[];
-      this.player2Array=[];
-      this.totalArray = [];
-      ticTac.winnerFound = false;
-      console.log("reset was called!!!!!");
-      console.log(this.player1Array+"   "+this.player2Array);
-    },
-
-    //computer generate click id
-    computer:function(){
-      return 3;
     }
 
+    // checking if play1 going to win, click the 3rd one.
+    if (this.player1Array.length >= 2) {
+      console.log('checking winner 1 is going to win')
+      for (key in this.winner) {
+
+        let counter = 0 // to record how close to win. 2 ==1 left to win
+        let winArray = this.winner[key];
+        for (let i = 0; i < winArray.length; i++) {
+          for (let j = 0; j < this.player1Array.length; j++) {
+            if (+winArray[i] === +this.player1Array[j] ){
+              counter++;
+              break;
+            }
+          }
+
+        }
+        if (counter === 2) {
+          console.log("find player1 winner pattern:" +winArray);
+          for (let i = 0; i < winArray.length; i++) {
+            for (let j = 0; j < this.player1Array.length; j++) {
+              if (+winArray[i] === +this.player1Array[j])
+                break;
+            }
+
+            if ( !(this.checkClick( winArray[i]) )  )
+            return winArray[i];
+          }
+        }
+      }
+    }
+
+  //check and radom
+
+    // console.log("before radom tatal list"+this.totalArray);
+
+     for (let k=1; k<10; k++){
+      //  console.warn('checkClick for ' + k)
+       if (!this.checkClick(k )){
+         remain.push(k);
+       }
+
+    }
+
+
+  //***************radom part
+  console.log('Radom from AI: remain list: '+remain);
+  let r = remain[Math.floor(Math.random()*remain.length)];
+  if (!this.checkClick(r))
+  return r;
+
+
+
+
   }
+
+}
